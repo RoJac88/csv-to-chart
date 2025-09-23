ae22cfbdfd7a139687d90cddd73f9e3de2249a50
Rodrigo Jacob <rodrigo.jacob@gmail.com>
2025-09-23 09:38:12 -0300

    new format

--- src/main.js
+++ src/main.js
@@ -4,12 +4,36 @@ import { Chart } from 'chart.js/auto';
 
 let chart = null;
 
-function fmtTime(item) {
-  const date = new Date(item.timestamp * 1000);
+function fmtTime(timestamp) {
+  const date = new Date(timestamp * 1000);
   const h = date.getHours().toString().padStart(2, '0');
   const m = date.getMinutes().toString().padStart(2, '0');
   const s = date.getSeconds().toString().padStart(2, '0');
   return `${h}:${m}:${s}`
+}
+
+// pid,timestamp,ram,vram
+
+function createDatasets(data) {
+  const sets = {};
+  const labels = [];
+  for (const entry of data) {
+    const { pid, timestamp, ram } = entry;
+    if (!labels.includes(timestamp)) {
+      labels.push(timestamp);
+    }
+    if (pid in sets) {
+      console.log('pid in datasets')
+      sets[pid].data.push(ram)
+    } else {
+      console.log('pid not in datasets')
+      sets[pid] = {
+        label: `RAM usage for pid ${pid}`,
+        data: [ram],
+      }
+    }
+  }
+  return { labels, datasets: Object.values(sets) };
 }
 
 function onData(results) {
@@ -18,26 +42,21 @@ function onData(results) {
     console.warn(errors);
     return;
   }
+  const { labels, datasets } = createDatasets(data);
+  console.log({ labels, datasets });
   if (!chart) {
     const el = document.getElementById('chart');
-    console.log(el)
     chart = new Chart(el, {
       type: 'line',
       data: {
-        labels: data.map(fmtTime),
-        datasets: [{
-          label: 'Memory usage in KB',
-          data: data.map((item) => item.usage),
-        }]
+        labels: labels.map(fmtTime),
+        datasets,
       }
     })
   } else {
     chart.data = {
-      labels: data.map((item) => item.timestamp),
-      datasets: [{
-        label: 'Memory usage in KB',
-        data: data.map((item) => item.usage),
-      }]
+      labels: labels.map(fmtTime),
+      datasets,
     }
     chart.update();
   }
