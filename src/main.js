db824349bba6e7c28512c94d387eaac71edb7c3a
Rodrigo Jacob <rodrigo.jacob@gmail.com>
2025-09-23 13:50:59 -0300

    add radio buttons

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import './style.css'
 import { parse } from 'papaparse';
 import { Chart } from 'chart.js/auto'; 
 
+const radio = document.getElementById('radio');
 let chart = null;
+let rawData = null;
 
 function fmtTime(timestamp) {
   const date = new Date(timestamp * 1000);
@@ -23,10 +25,8 @@ function createDatasets(data) {
       labels.push(timestamp);
     }
     if (pid in sets) {
-      console.log('pid in datasets')
       sets[pid].data.push(ram)
     } else {
-      console.log('pid not in datasets')
       sets[pid] = {
         label: `RAM usage for pid ${pid}`,
         data: [ram],
@@ -36,13 +36,39 @@ function createDatasets(data) {
   return { labels, datasets: Object.values(sets) };
 }
 
+function createAggregate(data) {
+  const values = new Map();
+  const labels = [];
+  for (const entry of data) {
+    const { timestamp, ram } = entry;
+    if (!labels.includes(timestamp)) {
+      labels.push(timestamp);
+    }
+    const exists = values.get(timestamp);
+    if (exists) {
+      values.set(timestamp, exists + ram);
+    } else {
+      values.set(timestamp, ram);
+    }
+  }
+  const datasets = [{
+    label: 'Total RAM usage',
+    data: [...values.values()],
+  }]
+  return { labels, datasets };
+}
+
 function onData(results) {
   const { data, errors } = results;
   if (errors.length) {
     console.warn(errors);
     return;
   }
-  const { labels, datasets } = createDatasets(data);
+  radio.style.display = 'block';
+  rawData = data;
+  const radioInput = document.querySelector('input[type=radio]:checked');
+  const aggregate = radioInput?.value === 'agg';
+  const { labels, datasets } = aggregate ? createAggregate(data) : createDatasets(data);
   console.log({ labels, datasets });
   if (!chart) {
     const el = document.getElementById('chart');
@@ -82,4 +108,19 @@ function handleFileChange(event) {
 const csvInput = document.getElementById('csv-input');
 
 csvInput.addEventListener('change', handleFileChange);
+
+const radios = document.querySelectorAll('input[type=radio]');
+for (const input of radios) {
+  input.addEventListener('change', (ev) => {
+    if (!rawData) return;
+    if (!chart) return;
+    const aggregate = ev.target.value === 'agg';
+    const { labels, datasets } = aggregate ? createAggregate(rawData) : createDatasets(rawData);
+    chart.data = {
+      labels: labels.map(fmtTime),
+      datasets,
+    }
+    chart.update();
+  })
+}
 
